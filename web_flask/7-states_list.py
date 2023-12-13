21d67c478c431df8b9f4a4a92639db65eee5b868
GTDeschamps <g.deschamps@hotmail.fr>
2023-12-13 10:16:46 +0100

    Task 8 & 9 : State and City flask script

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,7 +3,6 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 from sqlalchemy.orm import scoped_session
 
 
@@ -15,12 +14,13 @@ def teardown_db(exception):
     """Remove the current SQLAlchemy Session."""
     storage.close()
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
 
-    return render_template('cities_by_states.html', states=sorted_states)
+    return render_template('states_list.html', states=sorted_states)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
