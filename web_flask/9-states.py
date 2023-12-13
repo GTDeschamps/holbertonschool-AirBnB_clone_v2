3d26b87878543a83996332b92e2768d76494c5ac
GTDeschamps <g.deschamps@hotmail.fr>
2023-12-13 14:56:36 +0100

    Task 10: States by states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def display_states():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('states.html', states=sorted_states)
+
+
+@app.route('/states/<state_id>', strict_slashes=False)
+def display_state_cities(state_id):
+    state = storage.get(State, state_id)
+    if state:
+        cities = sorted(state.cities, key=lambda city: city.name)
+        return render_template('state_cities.html', state=state, cities=cities)
+    else:
+        return render_template('not_found.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
