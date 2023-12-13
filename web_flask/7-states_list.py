0646d83f33fe38acf436331bc682b0b2629da027
GTDeschamps <g.deschamps@hotmail.fr>
2023-12-13 20:44:07 +0100

    Correction of Task 8 with the good html name inside the code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,7 +5,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from sqlalchemy.orm import scoped_session
+from markupsafe import escape
 
 
 app = Flask(__name__)
@@ -13,10 +13,11 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    """display HTML page: (inside the tag BODY)"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
 
-    return render_template('states_list.html', states=sorted_states)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
