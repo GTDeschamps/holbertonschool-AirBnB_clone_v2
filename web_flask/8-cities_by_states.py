91abe9c12b8ce0d6f22de1ac15da7f1dfd9469c2
GTDeschamps <g.deschamps@hotmail.fr>
2023-12-15 15:41:42 +0100

    Update Task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,29 +5,17 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
-from markupsafe import escape
 
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display HTML page: (inside the tag BODY)"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-
-    return render_template('7-states_list.html', states=sorted_states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """display cities_by_states"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
+    state_list = storage.all(State).values()
 
-    return render_template('8-cities_by_states.html', states=sorted_states)
+    return render_template('8-cities_by_states.html', all_states=state_list)
 
 
 @app.teardown_appcontext
